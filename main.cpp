2c6709a6afcd984e30d4df0ea45dce2bf2568367
George F <78384472+Wozunubi@users.noreply.github.com>
2022-10-28 17:45:28 -0400

    start

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+
+class bankAcc {
+public:
+    std::string name;
+    std::string pin;
+    int bal;
+    bankAcc(std::string x, std::string y, int z);
+    void withdraw(int);
+    void deposit(int);
+    void showBal();
+};
+
+bankAcc::bankAcc(std::string x, std::string y, int z) {
+    name = std::move(x);
+    pin = std::move(y);
+    bal = z;
+}
+
+void bankAcc::withdraw(int val) {
+
+}
+
+void bankAcc::deposit(int val) {
+
+}
+
+void bankAcc::showBal() {
+    std::cout << "Current Balance: " << bal;
+}
+
+int main() {
+    std::vector<bankAcc> users;
+    std::string input, name, pin;
+
+    do {
+        std::cout << "A. Log-in\nB. Sign-up"; std::cin >> input;
+        std::cout << "Name:\n"; std::cin >> name;
+        std::cout << "PIN:\n"; std::cin >> pin;
+
+        if (input == "A") {
+            //Action
+            std::cout << "You Have Been Logged-In Successfully\n";
+            break;
+        } else if (input == "B") {
+            //Action
+            std::cout << "Your Account Has Been Initialized Successfully\n";
+        }
+    } while (true);
+
+    return 0;
+}
