a82c507a650482942625030a23e0466be1d1db6b
George F <78384472+Wozunubi@users.noreply.github.com>
2022-10-30 00:23:08 -0400

    withdraw/deposit/balance

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ public:
     std::string pin;
     int bal;
     bankAcc(std::string x, std::string y, int z);
-    void withdraw(int);
-    void deposit(int);
+    void withdraw();
+    void deposit();
     void showBal();
+    void editAcc();
 };
 
 bankAcc::bankAcc(std::string x, std::string y, int z) {
@@ -20,16 +21,37 @@ bankAcc::bankAcc(std::string x, std::string y, int z) {
     bal = z;
 }
 
-void bankAcc::withdraw(int val) {
+void bankAcc::withdraw() {
+    int val;
 
+    std::cout << "How Much Do You Want to Withdraw\n"; std::cin >> val;
+
+    if (val <= bal) {
+        bal -= val;
+        std::cout << "Fund Withdraw Successful\n";
+    } else {
+        std::cout << "The Account Does Not Have Enough Funds\n";
+    }
+    showBal();
 }
 
-void bankAcc::deposit(int val) {
+void bankAcc::deposit() {
+    int val;
 
+    std::cout << "How Much Do You Want to Deposit\n"; std::cin >> val;
+
+    bal += val;
+
+    std::cout << "Fund Deposit Successful\n";
+    showBal();
 }
 
 void bankAcc::showBal() {
-    std::cout << "Current Balance: " << bal;
+    std::cout << "Current Balance is $" << bal << "\n";
+}
+
+void bankAcc::editAcc() {
+    // do stuff
 }
 
 int main() {
@@ -45,34 +67,40 @@ int main() {
         std::cout << "PIN:\n"; std::cin >> pin;
 
         if (input == "A") {
-            //Action
-            std::cout << "You Have Been Logged-In Successfully\n";
-            do {
-                std::cout << "A. Withdraw\nB. Deposit\nC. Current Balance\nD. Edit Account\nE. Delete Account\nF. Back\n"; std::cin >> input;
+            if (users.find(name) == users.end()) {
+                std::cout << "The User Does Not Exist\n";
+            } else if (users[name]->pin != pin) {
+                std::cout << "The PIN is Incorrect\n";
+            } else {
+                std::cout << "You Have Been Logged-In Successfully\n";
+                do {
+                    std::cout << "A. Withdraw\nB. Deposit\nC. Current Balance\nD. Edit Account\nE. Delete Account\nF. Back\n"; std::cin >> input;
 
-                if (input == "A") {
-
-                } else if (input == "B") {
-
-                } else if (input == "C") {
-
-                } else if (input == "D") {
-
-                } else if (input == "E") {
-
-                } else if (input == "F") {
-                    break;
-                }
-            } while (true);
+                    if (input == "A") {
+                        users[name]->withdraw();
+                    } else if (input == "B") {
+                        users[name]->deposit();
+                    } else if (input == "C") {
+                        users[name]->showBal();
+                    } else if (input == "D") {
+                        users[name]->editAcc();
+                    } else if (input == "E") {
+                        std::cout << "Confirm Delete by Typing PIN\n"; std::cin >> input;
+                        if (users[name]->pin == pin) {
+                            // delete
+                        }
+                    } else if (input == "F") {
+                        break;
+                    }
+                } while (true);
+            }
         } else if (input == "B") {
             users.insert({name, new bankAcc(name, pin, 0)});
             std::cout << "Your Account Has Been Initialized Successfully\n";
         }
     } while (true);
 
-    std::cout << users["George"]->name;
-
-    std::cin >> input;
+    // store to csv
 
     return 0;
 }
